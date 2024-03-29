0b1d768b676d38be91772c7cfbb99292578a78dd
Dmytro Yarechuk <retrodn14@gmail.com>
2024-03-29 18:21:39 +0200

    created landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function moveZeros(arr) {
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      newArr.push(arr[i]);
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr;
+}
+
+console.log(moveZeros([true, 0, 'abc', '1', 12]));
