9ee08b80a41019c56628e7a9f3801a41cd93fc27
Dmytro Yarechuk <retrodn14@gmail.com>
2024-03-29 18:24:59 +0200

    make build

---
+++ index.6274fdd0.js
@@ -0,0 +1,2 @@
+console.log(function(n){for(var o=[],r=0;r<n.length;r++)0!==n[r]&&o.push(n[r]);for(var e=0;e<n.length;e++)0===n[e]&&o.push(n[e]);return o}([!0,0,"abc","1",12]));
+//# sourceMappingURL=index.6274fdd0.js.map
